797a429cc38b411467350bc50ed037ca538a8bfa
stanimeros <pantelisstanimeros@outlook.com>
2025-08-01 11:52:56 +0300

    Add Firebase SDK and remove App.css file

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU37Hg8zZP5uROFaNSnKpn1CwJ10sww_8",
+  authDomain: "stanimeros-dev.firebaseapp.com",
+  projectId: "stanimeros-dev",
+  storageBucket: "stanimeros-dev.firebasestorage.app",
+  messagingSenderId: "473688963752",
+  appId: "1:473688963752:web:ec8fe54a00b18c5536609d",
+  measurementId: "G-Y1LFMK4QH2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
